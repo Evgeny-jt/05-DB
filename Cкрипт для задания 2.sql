dd49cf0ac1bac727b683bded42faed4c9c5c4e26
Evgeny Troshin <dylan_jt@mail.ru>
2024-07-17 01:54:22 +0300

    Initial commit

---
+++ Cкрипт для задания 2.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS МузыкальныеЖанры (
+	жанр_id SERIAL PRIMARY KEY,
+	жанр VARCHAR(20) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS Исполнители (
+	исполнитель_id SERIAL PRIMARY KEY,
+	исполнитель VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS ЖанрыИсполнители (
+	жанры INTEGER REFERENCES МузыкальныеЖанры(жанр_id),
+	исполнители INTEGER REFERENCES Исполнители(исполнитель_id),
+	CONSTRAINT жанрисп PRIMARY KEY (жанры, исполнители)
+);
+
+
+CREATE TABLE IF NOT EXISTS Альбомы (
+	альбом_id SERIAL PRIMARY KEY,
+	альбом VARCHAR(50) NOT null,
+	год VARCHAR(4) NOT null
+);
+
+
+CREATE TABLE IF NOT EXISTS ИсполнительАльбом (
+	исполнители INTEGER REFERENCES Исполнители(исполнитель_id),
+	альбомы INTEGER REFERENCES Альбомы(альбом_id),
+	CONSTRAINT СвязьИсполнителейАльбомов PRIMARY KEY (исполнители, альбомы)
+);
+
+
+CREATE TABLE IF NOT EXISTS Треки (
+	трек_id SERIAL PRIMARY KEY,
+    альбом_id INTEGER REFERENCES Альбомы(альбом_id),
+	трек VARCHAR(50) NOT null,
+	продолжительность  INTEGER NOT null
+);
+
+
+CREATE TABLE IF NOT EXISTS Сборники (
+	сборник_id SERIAL PRIMARY KEY,
+	сборник VARCHAR(40) NOT null,
+	год VARCHAR(40) NOT null
+);
+
+
+CREATE TABLE IF NOT EXISTS СборникиТреков (
+	сборники INTEGER REFERENCES Сборники(сборник_id),
+	треки INTEGER REFERENCES Треки(трек_id),
+	CONSTRAINT pk PRIMARY KEY (сборники, треки)
+);
