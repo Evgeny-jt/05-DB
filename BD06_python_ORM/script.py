7a47c88b7a77d7fe421594b66e92b0e2da8a1ad3
Evgeny Troshin <dylan_jt@mail.ru>
2024-08-02 22:51:03 +0300

    Дороботанная 6ая домашняя работа v3

--- BD06_python_ORM/script.py
+++ BD06_python_ORM/script.py
@@ -40,33 +40,3 @@ def leveling(words, word):   # Выравнваем длинну слова до
 if __name__ == '__main__':
     search = input('Для вывода списка купленных книг издатля. Введите автора или его id: ') #Просим клиента ввести имя или айди публициста и данные сохраняем в переменную
     get_shops(search=search) #Вызываем функцию получения данных из базы, передавая в функцию данные, которые ввел пользователь строкой выше
-
-    # DSN = 'postgresql://postgres:2024p09114@localhost:5432/BD06'
-    # engine = sqlalchemy.create_engine(DSN)
-    #
-    # Session = sessionmaker(bind=engine)
-    # session = Session()
-
-    # publisher = input('Для вывода списка купленных книг издатля. Введите автора или его id: ')
-    # print('У автора', publisher, 'купили : ')
-    #
-    # len_book = []
-    # len_shop = []
-    # len_prise = []
-    # for p, book, shop, prise in session.query(Publisher, Book.title, Shop.name, Sale.prise).join(Book).join(Stock).join(
-    #         Shop).join(Sale).filter(Publisher.name == publisher):
-    #     len_book.append(len(book))
-    #     len_shop.append(len(shop))
-    #     len_prise.append(len(str(prise)))
-    #
-    # for p, b, sh, pr, d in session.query(Publisher, Book.title, Shop.name, Sale.prise, Sale.date_sale).join(Book).join(
-    #         Stock).join(Shop).join(Sale).filter(Publisher.name == publisher):
-    #     new_b = leveling(words=len_book, word=b)
-    #     new_sh = leveling(words=len_shop, word=sh)
-    #     new_pr = leveling(words=len_prise, word=str(pr))
-    #     print(new_b, '|', new_sh, '|', new_pr, '|', d)
-
-    # session.close()
-
-
-
